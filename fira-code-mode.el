a46495464fafa9ca7863eb274d229c2a4b34b1b2
Jonathan Ming <jming@pkcsecurity.com>
2019-08-23 11:15:32 -0700

    Rename

--- fira-code-mode.el
+++ fira-code-mode.el
@@ -1,29 +1,29 @@
-;;; fira-code.el --- Fira Code ligatures using prettify-symbols.
+;;; fira-code-mode.el --- Minor mode for Fira Code ligatures using prettify-symbols.
 
 ;; This file is not part of GNU Emacs.
 
 ;;; License:
 
-;; fira-code.el is free software; you can redistribute it and/or modify
+;; fira-code-mode is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
 ;; the Free Software Foundation, either version 3 of the License, or
 ;; (at your option) any later version.
 
-;; fira-code.el is distributed in the hope that it will be useful,
+;; fira-code-mode is distributed in the hope that it will be useful,
 ;; but WITHOUT ANY WARRANTY; without even the implied warranty of
 ;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 ;; GNU General Public License for more details.
 
 ;; You should have received a copy of the GNU General Public License
-;; along with fira-code.el.  If not, see <http://www.gnu.org/licenses/>.
+;; along with fira-code-mode.  If not, see <http://www.gnu.org/licenses/>.
 
 ;;; Commentary:
-;; A simple package for Fira Code ligatures, built from these instructions:
+;; Minor mode for Fira Code ligatures, built from these instructions:
 ;; https://github.com/tonsky/FiraCode/wiki/Emacs-instructions#using-prettify-symbols
 ;; 
 ;; Has some of my customizations baked into it still.
 ;; 
-;; Requires installing the Fira Code Symbol font from here:
+;; NOTE: Requires installing the Fira Code Symbol font from here:
 ;; https://github.com/tonsky/FiraCode/issues/211#issuecomment-239058632
 
 ;;; Code:
@@ -93,4 +93,4 @@
   (set-fontset-font t '(#Xe100 . #Xe16f) "Fira Code Symbol"))
 
 (provide 'fira-code-mode)
-;;; fira-code.el ends here
+;;; fira-code-mode.el ends here
