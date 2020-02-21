7723919d8d5577e86a93ecc733ef2a9cb05f5003
Naoya Yamashita <conao3@gmail.com>
2020-02-21 17:56:56 +0900

    Update fira-code-mode.el
    Approve suggestion
    
    Co-Authored-By: Jonathan Ming <jonathan@reveelgroup.com>

--- fira-code-mode.el
+++ fira-code-mode.el
@@ -107,7 +107,7 @@ will need to disable and re-enable the mode in order for the edits to take effec
 ;;;###autoload
 (define-minor-mode fira-code-mode
   "Fira Code ligatures minor mode"
-  :lighter " \xe15b"
+  :lighter "  \xe15b"
   :group 'fira-code-ligatures
   (when (not (display-graphic-p))
     (display-warning '(fira-code-ligatures) "Warning: fira-code-mode probably won't work for non-graphical displays!"))
